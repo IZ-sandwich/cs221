8869000b52f3ec5f15bca0fc6eb3a344d92e064a
Chin Chen <m4u9a@annacis.ugrad.cs.ubc.ca>
2017-03-28 17:46:58 -0700

    Organized files and updated README

--- pp3/generator1.cc
+++ pp3/generator1.cc
@@ -12,27 +12,15 @@ if (argc != 2) // remember, argv[0] is the program name
 		cerr << "Wrong number of arguments!" << endl;
 		return 1;
 	}
+	srand(10);
 	unsigned int n = atoi(argv[1]);
-
-	vector<unsigned long long> s;
-	for (unsigned int i=0; i< n/3; i++) {
-		s.push_back(i);
-	}
-	random_shuffle(s.begin(),s.end());
-
-	for(unsigned int i = 0; i < n/3; i++) {
-		cout << "I " << s[i] << endl;
-	}
-	random_shuffle(s.begin(),s.end());
 	
-	for (unsigned int i=0; i< n/3; i++) {
-		int k = s[1];  //search for the same value over and over again
-		//int k = 9000000; //search for a value not in the dict
-		cout << "F " << k << endl;
+	for(unsigned int i = 1; i < n/2; i++) {
+		cout << "I " << i << endl;
 	}
 
-	for (unsigned int i=0; i<n/3; i++) {
-		unsigned int k = s[i]; 	//search for a random value
+	for (unsigned int i = 1; i < n/2; i++) {
+		unsigned int k = rand() % (n/2 - 1) + 1; 	//search for a random value
 		cout << "F " << k << endl;
 	}
 	return 0;
--- pp3/generator4.cc
+++ pp3/generator4.cc
@@ -14,8 +14,13 @@ if (argc != 2) // remember, argv[0] is the program name
 	}
 	unsigned int n = atoi(argv[1]);
 	
-	for(unsigned int i = 1; i < n; i++) {
+	for(unsigned int i = 1; i < n/2; i++) {
 		cout << "I " << i << endl;
 	}
+	
+	for(unsigned int i = 1; i < n/2; i++) {
+		cout << "F " <<  n/3 << endl;
+	}
+	
 	return 0;
 }
